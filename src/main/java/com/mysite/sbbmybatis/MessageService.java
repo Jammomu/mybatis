3d8bb72c28aa45a22ad7ee9a9450ee49bb655568
khj(YS) <pridom03@gmail.com>
2024-10-01 16:55:03 +0900

    Message create

---
+++ src/main/java/com/mysite/sbbmybatis/MessageService.java
@@ -0,0 +1,18 @@
+package com.mysite.sbbmybatis;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MessageService {
+	@Autowired
+	private MessageMapper messageMapper;
+	
+	public Message getMessage(Integer id) {
+		return messageMapper.getMessageById(id);
+	}
+	
+	public void createMessage(Message message) {
+		messageMapper.insertMessage(message);
+	}
+}
